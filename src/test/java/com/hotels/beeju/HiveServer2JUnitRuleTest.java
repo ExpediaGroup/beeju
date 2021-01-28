2f990f8e76c40440c8340adf1afb2e4928a2a8c8
massdosage <awoodhead@expediagroup.com>
2021-01-28 15:52:13 +0000

    commit to check that master->main has no impact

--- src/test/java/com/hotels/beeju/HiveServer2JUnitRuleTest.java
+++ src/test/java/com/hotels/beeju/HiveServer2JUnitRuleTest.java
@@ -19,7 +19,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.hadoop.hive.conf.HiveConf;
