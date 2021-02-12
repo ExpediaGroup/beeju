fed9a92ee5cf20d0245245e971a68090f0d2f5b8
nvitucci <nvitucci@expediagroup.com>
2021-02-12 14:06:27 +0000

    Added metastore conf

--- src/main/java/com/hotels/beeju/extensions/HiveMetaStoreJUnitExtension.java
+++ src/main/java/com/hotels/beeju/extensions/HiveMetaStoreJUnitExtension.java
@@ -15,6 +15,8 @@
  */
 package com.hotels.beeju.extensions;
 
+import static org.apache.hadoop.hive.metastore.conf.MetastoreConf.ConfVars.CONNECT_URL_KEY;
+
 import java.util.Map;
 
 import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
@@ -61,6 +63,7 @@ public class HiveMetaStoreJUnitExtension extends BeejuJUnitExtension {
 
   @Override
   public void beforeEach(ExtensionContext context) throws Exception {
+    System.clearProperty(CONNECT_URL_KEY.getVarname());
     super.beforeEach(context);
     hiveMetaStoreCore.initialise();
   }
--- src/main/java/com/hotels/beeju/extensions/HiveServer2JUnitExtension.java
+++ src/main/java/com/hotels/beeju/extensions/HiveServer2JUnitExtension.java
@@ -15,6 +15,8 @@
  */
 package com.hotels.beeju.extensions;
 
+import static org.apache.hadoop.hive.metastore.conf.MetastoreConf.ConfVars.CONNECT_URL_KEY;
+
 import java.util.Map;
 
 import org.apache.hive.jdbc.HiveDriver;
@@ -55,6 +57,7 @@ public class HiveServer2JUnitExtension extends BeejuJUnitExtension {
 
   @Override
   public void beforeEach(ExtensionContext context) throws Exception {
+    System.clearProperty(CONNECT_URL_KEY.getVarname());
     hiveServer2Core.startServerSocket();
     super.beforeEach(context);
     hiveServer2Core.initialise();
